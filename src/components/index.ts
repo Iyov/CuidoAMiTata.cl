275c8e5afa0905ce6dc190d781732a3c66599330
FcoBarrientos <francisco.barrientos@meir.cl>
2026-02-14 12:56:50 -0300

    Fix index.ts export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
-// DEPRECATED: This barrel export file is no longer used to avoid code splitting issues.
-// Import components directly from their individual files instead.
-// Example: import { Button } from './components/Button';
+export * from './Alert';
+export * from './Button';
+export * from './Card';
+export * from './Input';
 
-// This file is kept for backwards compatibility but should not be used in new code.
